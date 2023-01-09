2ef5fd1f6ba3a3f05c1881c95bed25a4e1924693
tomo <garudaputro@gmail.com>
2023-01-09 16:50:13 +0800

    added api

--- api/config/database.js
+++ api/config/database.js
@@ -4,10 +4,10 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'eunoya'),
+      database: env('DATABASE_NAME', 'eunoya_react'),
       user: env('DATABASE_USERNAME', 'root'),
       password: env('DATABASE_PASSWORD', ''),
-      ssl: env.bool('DATABASE_SSL', true),
+      ssl: env.bool('DATABASE_SSL', false),
     },
   },
 });
--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -19,13 +19,13 @@ const Categories = () => {
   return (
     <div className="section">
       <h5 className="title">Categories</h5>
-      <div className="category flex gap-3 justify-center">
+      <div className="category flex gap-8 justify-center">
         {data.map((item) => (
           <Link to={`products/${item.id}`}
             key={item.id}
             className="bg-card p-4 rounded-xl flex gap-2 items-center transition border border-primary hover:-translate-y-1 hover:smooth-shadow "
           >
-            <img src={ CategoriesWi} className='w-8 h-8' alt="Logo" />
+            <img src={CategoriesWi} className='w-8 h-8' alt="Logo" />
             {item.category}
           </Link>
         ))}
